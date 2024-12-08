356eea865d177fa6b8ba7786fd53211e73c3b69b
Debarup <87480898+debarup24@users.noreply.github.com>
2024-12-08 22:22:15 +0530

    news portal

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from './ui/navigation-menu'
+
 
 const Navbar = () => {
   return (
@@ -8,6 +11,47 @@ const Navbar = () => {
             <div className='text-xl font-bold'>
                 <Link href="">Pocket News</Link>
             </div>
+          {/* <div>
+            <Button variant="default" className='bg-slate-50 hover:bg-slate-900 text-black hover:text-white px-7'>Button</Button>
+           </div> */}
+
+           <div>
+ <NavigationMenu className='hidden lg:flex'>
+   <NavigationMenuList>
+     <NavigationMenuItem>
+       <NavigationMenuLink href='/news' >News</NavigationMenuLink>
+     </NavigationMenuItem>
+     <NavigationMenuItem>
+       <NavigationMenuTrigger>Category</NavigationMenuTrigger>
+       <NavigationMenuContent>
+         <NavigationMenuLink>
+          <ul className='text-gray-600 shadow-md py-5 px-5 space-y-2'>
+            <li>
+            <NavigationMenuLink href='/national-news' >National News</NavigationMenuLink>
+            </li>
+
+            <li>
+            <NavigationMenuLink href='/international-news' >International News</NavigationMenuLink>
+            </li>
+          </ul>
+        </NavigationMenuLink>
+       
+      </NavigationMenuContent>
+      </NavigationMenuItem>
+
+      <NavigationMenuItem>
+       <NavigationMenuLink href='/contact' >Contact</NavigationMenuLink>
+     </NavigationMenuItem>
+
+     <NavigationMenuItem>
+       <NavigationMenuLink href='/about' >About</NavigationMenuLink>
+     </NavigationMenuItem>
+
+
+  </NavigationMenuList>
+</NavigationMenu>
+
+           </div>
 
         </nav>
     </header>
