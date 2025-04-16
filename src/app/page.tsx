1979f70ec7979aeb2b5f5f18d12471dc1a1ace3a
Debarup <87480898+debarup24@users.noreply.github.com>
2025-04-16 21:49:18 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12"> 
       <Banner/>
      <div className="my-8">
-      <h2 className="text-2xl font-bold mb-8"> Latest <span className="text-red-600">News</span></h2>
+      <h2 className="text-2xl font-bold mb-8"> Latest <span className="text-red-600">NEWS</span></h2>
       <div className="grid md:grid-cols-3 lg:grid-cols-3 grid-cols-1 gap-5 justify-between">
         {
           news.slice(0, 3).map((item : NewsItems)=> (
